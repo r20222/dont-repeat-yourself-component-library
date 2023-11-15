0b1d91a8b03cc6afa2178f04af51c62eb7a0b7e3
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-11-15 16:06:03 +0100

    begin opzet carrousel

--- src/components/image.svelte
+++ src/components/image.svelte
@@ -1,20 +1,45 @@
 <script>
     export let data
 </script>
-
-{#each data as img}
-    <div class="image-container">
-        <img alt="" src="{img.url}">
-    </div>
-{/each}
+<section>
+    <ul>
+        {#each data as img}
+            <li>
+                <img alt="" src="{img.url}">
+            </li>
+        {/each}
+    </ul>
+</section>
 
 <style>
-    img{
+    section {
+        /* grid-area: auto; */
+        width: 100%;
+        overflow: hidden;
+    }
+    ul {
+        display: flex;
+        overflow-x: auto;
+        scroll-snap-type: x mandatory;
+        scroll-behavior: smooth;
+        -webkit-overflow-scrolling: touch;
+    }
+    li {
+        width: 100%;
+        scroll-snap-align: start;
+        flex-shrink: 0;
+        transform-origin: center center;
+        overflow-y: hidden;
+
+    }
+    img {
         width: 100%; /* veranderd van 10rem naar 100% om het mooi in de container te laten passen */
         height: 100%;
         object-fit: cover;
     }
-    .image-container{
-        grid-area: auto;  
+
+    img::before {
+        
     }
+    
 </style>
