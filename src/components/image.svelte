f3881461686fe761ee84930ee0468361ddb9d9a0
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-11-21 10:46:33 +0100

    button werkt

--- src/components/image.svelte
+++ src/components/image.svelte
@@ -1,18 +1,26 @@
 <script>
     import { onMount } from 'svelte';
     export let data
-    console.log(data[0].image[0])
-    // onMount(() =>{
-    //     var buttonLeft = document.querySelector("button:first-of-type")
-    //     var buttonRight = document.querySelector("button:last-of-type")
-    //     buttonLeft.addEventListener("click", () => { })
-    //     buttonRight.addEventListener("click", () => { })
+    onMount(() =>{
+        var buttonLeft = document.querySelector("button:first-of-type")
+        var buttonRight = document.querySelector("button:last-of-type")
+        var scroller = document.querySelector(".scroller")
+        const itemWidth = document.querySelector('img').clientWidth;
+        console.log(scroller)
+        buttonLeft.addEventListener("click", () => {
+            console.log(itemWidth)
+            scroller.scrollBy({left: -100, top: 0, behavior: 'smooth'})
+        })
+        buttonRight.addEventListener("click", () => {
+            console.log("hello")
+            scroller.scrollBy({left: 100, top: 0, behavior: 'smooth'})
+        })
         
-    // })
+    })
 </script>
 <section>
-    <!-- <button></button> -->
-    <ul>
+    <button></button>
+    <ul class="scroller">
         {#each data as img}
             <li>
                 <picture>
@@ -22,14 +30,14 @@
             </li>
         {/each}
     </ul>
-    <!-- <button></button> -->
+    <button></button>
 </section>
 
 <style>
     section {
         grid-area: image;
         width: 100%;
-        height: 100%;
+        height: 500px;
         overflow: hidden;
     }
     ul {
@@ -52,7 +60,7 @@
         height: 30em;
         object-fit: fill;
     }
-    /* button {
+    button {
         background-color: white;
         width: 5em;
         height: 5em;
@@ -66,7 +74,7 @@
     }
     
     ul+button {
-        left: 32em;
         top: 9em;   
-    } */
+        left: 20em;
+    }
 </style>
