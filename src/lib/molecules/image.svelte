a5f8d9f2b5c3b82f16017f417b68a0706ae2cf18
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-11-29 15:53:02 +0100

    css aanpassingen

--- src/lib/molecules/image.svelte
+++ src/lib/molecules/image.svelte
@@ -16,22 +16,22 @@
     })
 </script>
 <section>
-    <button title="carrousel scroll left">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M15.293 3.293 6.586 12l8.707 8.707 1.414-1.414L9.414 12l7.293-7.293-1.414-1.414z"/></svg>
-    </button>
-    <ul class="scroller">
-        {#each data as img}
-            <li>
-                <picture>
-                    <source srcset="{img.image[1].url}" type="image/webp">
-                    <img alt="" src="{img.image[0].url}" id="{img.length}" loading="lazy">     
-                </picture>
-            </li>
-        {/each}
-    </ul>
-    <button title="carrousel scroll right">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z"/></svg>
-    </button>
+        <button class="control control--left" title="carrousel scroll left">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M15.293 3.293 6.586 12l8.707 8.707 1.414-1.414L9.414 12l7.293-7.293-1.414-1.414z"/></svg>
+        </button>
+        <ul class="scroller">
+            {#each data as img}
+                <li>
+                    <picture>
+                        <source srcset="{img.image[1].url}" type="image/webp">
+                        <img alt="" src="{img.image[0].url}" id="{img.length}" loading="lazy">     
+                    </picture>
+                </li>
+            {/each}
+        </ul>
+        <button class="control control--right" title="carrousel scroll right">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z"/></svg>
+        </button>
 </section>
 
 <style>
@@ -41,40 +41,56 @@
         height: 500px;
         overflow: hidden;
         display: flex;
+        position: relative;
     }
     ul {
+        width: 100vw;
         display: flex;
         overflow-x: auto;
         scroll-snap-type: x mandatory;
         scroll-behavior: smooth;
         -webkit-overflow-scrolling: touch;
-        position: absolute;
+        padding: 0;
+
     }
     li {
         scroll-snap-align: start;
         flex-shrink: 0;
         transform-origin: center center;
         overflow-y: hidden;
-
+        width: 100%;
+    }
+    picture{
+        display: block;
+        position: relative;
+        height: 100%;
+        width: 100%;
     }
     img {
-        object-fit: fill;
+        height: 100%;
+        object-fit: cover;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
     }
     button {
         background-color: white;
+        border: 1px solid black;
+        /* box-shadow: 5px 5px; */
         width: 5em;
         height: 5em;
         border-radius: 5em;
-        position: relative;
-        left: 0;
-        top: 10em;
+        position: absolute;
         display: block;
         border: 1px white solid;
         z-index: 2;
+        top: 50%;
     }
-    
-    ul+button {
-        top: 10em;   
-        
+    .control--left{
+        left: 0; 
+    }
+    .control--right{
+        right: 0;
     }
 </style>
